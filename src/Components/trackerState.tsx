4309ec2dde51e6d4fe044730cced973f9533359c
rooshanahmed <rooshan.ahmed@hotmail.com>
2020-09-12 04:03:23 +0500

    Context Updated

--- src/Components/trackerState.tsx
+++ src/Components/trackerState.tsx
@@ -1,14 +1,38 @@
-import React, { createContext, useReducer } from 'react';
+import React, { createContext, useReducer } from "react";
+import { IState, ITrans, IAction } from "../Types/Interface";
 
-const initialState = {
-    transactions: [
+const initialState: IState = {
+  transactions: [],
+};
 
-    ]
+export const Tracker = createContext<IState | any>(initialState);
+
+function reducer(state: IState, action: IAction) {
+  switch (action.type) {
+    case "ADD":
+      return {
+        ...state,
+        transactions: [action.payload, ...state.transactions],
+      };
+
+    case "DEL":
+      return {
+        ...state,
+        transactions: state.transactions.filter(
+          (transaction: ITrans) => transaction.id !== action.payload.id
+        ),
+      };
+
+    default:
+      return state;
+  }
 }
 
-export const Tracker = createContext(initialState);
-
-
-function reducer(state:number , action:string) {
-    
+export function TrackerProvider(props: any): JSX.Element {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <Tracker.Provider value={{ state, dispatch }}>
+      {props.children}
+    </Tracker.Provider>
+  );
 }
