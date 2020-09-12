cd2968134a6353f57f33403c4cf21ab5c4a79618
rooshanahmed <rooshan.ahmed@hotmail.com>
2020-09-12 13:40:07 +0500

    App Updated

--- src/Components/IncomeExp.tsx
+++ src/Components/IncomeExp.tsx
@@ -1,5 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import CountUp from 'react-countup';
 import { Box , Paper } from '@material-ui/core';
 import { Tracker } from '../Context/trackerState';
 import {ITrans} from '../Types/Interface';
+
+const IncomeExp = () => {
+    const {state} = useContext(Tracker);
+
+    const amounts:Array<number> = state.transactions.map((transaction: ITrans) => transaction.amount);
+    const income = +amounts.filter((item) => item>0).reduce((acc,item) => (acc += item),0).toFixed(2);
+    const expense = +(amounts.filter((item) => item<0).reduce((acc,item) => (acc += item),0)*-1).toFixed(2);
+}
