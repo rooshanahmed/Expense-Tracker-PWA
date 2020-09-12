041118fe1e3702359279ae8a3549e47d365b729f
rooshanahmed <rooshan.ahmed@hotmail.com>
2020-09-12 13:42:35 +0500

    App Updated

--- src/Components/IncomeExp.tsx
+++ src/Components/IncomeExp.tsx
@@ -1,13 +1,21 @@
-import React, { useContext } from 'react';
-import CountUp from 'react-countup';
-import { Box , Paper } from '@material-ui/core';
-import { Tracker } from '../Context/trackerState';
-import {ITrans} from '../Types/Interface';
+import React, { useContext } from "react";
+import CountUp from "react-countup";
+import { Box, Paper } from "@material-ui/core";
+import { Tracker } from "../Context/trackerState";
+import { ITrans } from "../Types/Interface";
 
 const IncomeExp = () => {
-    const {state} = useContext(Tracker);
+  const { state } = useContext(Tracker);
 
-    const amounts:Array<number> = state.transactions.map((transaction: ITrans) => transaction.amount);
-    const income = +amounts.filter((item) => item>0).reduce((acc,item) => (acc += item),0).toFixed(2);
-    const expense = +(amounts.filter((item) => item<0).reduce((acc,item) => (acc += item),0)*-1).toFixed(2);
-}
+  const amounts: Array<number> = state.transactions.map(
+    (transaction: ITrans) => transaction.amount
+  );
+  const income = +amounts
+    .filter((item) => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+  const expense = +(
+    amounts.filter((item) => item < 0).reduce((acc, item) => (acc += item), 0) *
+    -1
+  ).toFixed(2);
+};
