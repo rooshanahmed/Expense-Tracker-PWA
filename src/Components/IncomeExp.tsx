ea0f3d18119eb7dace1841216b4f2d77b08190b6
rooshanahmed <rooshan.ahmed@hotmail.com>
2020-09-13 09:52:38 +0500

    Updated

--- src/Components/IncomeExp.tsx
+++ src/Components/IncomeExp.tsx
@@ -18,4 +18,19 @@ const IncomeExp = () => {
     amounts.filter((item) => item < 0).reduce((acc, item) => (acc += item), 0) *
     -1
   ).toFixed(2);
+
+  return (
+    <>
+      <Box m={2} mt={1}>
+        <div className="inc-exp-container">
+          <Paper className="incBox">
+            <h4>Income</h4>
+            <p id="money-plus" className="money plus">
+              <CountUp start={0} end={income} duration={3.00} separator="," decimals={2} decimal="." prefix="$" />
+            </p>
+          </Paper>
+        </div>
+      </Box>
+    </>
+  )
 };
